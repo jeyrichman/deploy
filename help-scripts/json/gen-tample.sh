4b098edcf16b609ec56111c8ea309a94025e7725
puppet-staging <servicedesk@devellar.com>
2017-06-01 18:46:00 -0400

    sample script for json blocks creation

---
+++ help-scripts/json/gen-tample.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while read -r i;do \
+HOSTNAME=`echo $i | awk '{print $1}'`
+IP=`echo $i | awk '{print $2}'`
+DOMAIN=`echo $i | awk '{print $3}'`
+
+cat <<EOF >> out.txt
+                "$HOSTNAME" : {
+			"ip" : "$IP",
+			"domains" : { 
+			"$DOMAIN" : {
+				"le-ssl" : [ "www" ],
+				"ssl_disabled" : "true"
+				}				
+			}
+		},
+EOF
+done < list
