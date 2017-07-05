c166063bf0b6ef9fb07c0e9c2983cc3d0b01793a
puppet-staging <servicedesk@devellar.com>
2017-07-05 10:32:55 -0400

    Added new feature

--- help-scripts/json/gen-tample.sh
+++ help-scripts/json/gen-tample.sh
@@ -18,3 +18,17 @@ cat <<EOF >> out.txt
 		},
 EOF
 done < list
+
+
+while read -r i;do \
+        HOSTNAME=`echo $i | awk '{print $1}'`
+IP=`echo $i | awk '{print $2}'`
+DOMAIN=`echo $i | awk '{print $3}'`
+
+cat <<EOF >> puppet.txt
+./deploy-host.sh $IP $HOSTNAME.essay.sat wp-site
+EOF
+done < list
+
+cat out.txt
+cat puppet.txt
