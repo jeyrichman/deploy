092b6b2930cf887a5e5da176c8089a121acf2d28
Eugene <jeyraboy@gmail.com>
2017-07-18 17:49:36 -0400

    Create chng-prmts.sh
    simple script for change some parameters in wp-config

---
+++ bash/scripts_deploy/wordpress/chng-prmts.sh
@@ -0,0 +1,12 @@
+
+DOMAIN=`pwd | cut -d "/" -f 5 | sed 's/\./\-/g'`
+DB_USER=`cat wp-config.php | grep "DB_USER"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
+DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
+DB_HOST=`cat wp-config.php | grep "DB_HOST"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
+
+/usr/bin/mysql < $DOMAIN.sql
+sed -i "s/$DB_HOST/localhost/" wp-config.php
+sed -i "s/$DB_USER/root/" wp-config.php
+sed -i "s/$DB_PASSWORD/123123asd/" wp-config.php
+
+rm $DOMAIN.sql
