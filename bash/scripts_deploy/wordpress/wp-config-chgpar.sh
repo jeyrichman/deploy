0e3287db2ff8098b12433cbd30d4ee1c15f4c2d0
Eugene <jeyraboy@gmail.com>
2017-07-20 17:08:07 -0400

    Update wp-config-chgpar.sh

--- bash/scripts_deploy/wordpress/wp-config-chgpar.sh
+++ bash/scripts_deploy/wordpress/wp-config-chgpar.sh
@@ -3,9 +3,9 @@ DB_USER=`cat wp-config.php | grep "DB_USER"  | awk "{print $2}"  | awk -F", '" '
 DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
 DB_HOST=`cat wp-config.php | grep "DB_HOST"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
 
-/usr/bin/mysql < db-bak/$DOMAIN.sql
 sed -i "s/$DB_HOST/localhost/" wp-config.php
 sed -i "s/$DB_USER/root/" wp-config.php
 sed -i "s/$DB_PASSWORD/123123asd/" wp-config.php
 
-rm -rf db-bak/
+/usr/bin/mysql < $DOMAIN.sql
+rm -rf 
