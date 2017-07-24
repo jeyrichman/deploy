9c18b1eb8073f039690606627778cdf9cb5daa6c
Eugene <jeyraboy@gmail.com>
2017-07-24 17:52:02 -0400

    Update wp-config-chgpar.sh

--- bash/scripts_deploy/wordpress/wp-config-chgpar.sh
+++ bash/scripts_deploy/wordpress/wp-config-chgpar.sh
@@ -1,11 +1,10 @@
-DOMAIN=`pwd | cut -d "/" -f 5 | sed 's/\./\-/g'`
 DB_USER=`cat wp-config.php | grep "DB_USER"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
-DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
-DB_HOST=`cat wp-config.php | grep "DB_HOST"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
-sleep 2
-/bin/sed -i "s/$DB_HOST/localhost/" wp-config.php
 /bin/sed -i "s/$DB_USER/root/" wp-config.php
+DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
 /bin/sed -i "s/$DB_PASSWORD/123123asd/" wp-config.php
+DB_HOST=`cat wp-config.php | grep "DB_HOST"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
+/bin/sed -i "s/$DB_HOST/localhost/" wp-config.php
 
+DOMAIN=`pwd | cut -d "/" -f 5 | sed 's/\./\-/g'`
 /usr/bin/mysql < $DOMAIN.sql
 rm -rf 
