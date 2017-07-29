474089b11d791f28d4cc328ad2a8d3961bc4643e
Eugene <jeyraboy@gmail.com>
2017-07-29 13:54:19 -0400

    Update wp-config-chgpar.sh

--- bash/scripts_deploy/wordpress/wp-config-chgpar.sh
+++ bash/scripts_deploy/wordpress/wp-config-chgpar.sh
@@ -2,7 +2,7 @@ IP=$1
 
 cat <<'EOT' | ssh root@$IP /bin/bash
 cd /var/www/html/*
-PWD=`pwd | cut -d "/" -f 5 | sed 's/\./\-/g'`
+DOMAIN=`pwd | cut -d "/" -f 5 | sed 's/\./\-/g'`
 DB_USER=`cat wp-config.php | grep "DB_USER"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
 /bin/sed -i "s/$DB_USER/root/" wp-config.php
 DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
@@ -12,6 +12,6 @@ DB_HOST=`cat wp-config.php | grep "DB_HOST"  | awk "{print $2}"  | awk -F", '" '
 
 /usr/bin/mysql < *.sql
 rm -rf *sql
-sudo -u www bash /opt/scripts/commit.sh $PWD
-/usr/share/essay/alert/slack.sh "satellites" "changes commited by eugene.ryabchuk for satellite [SA-7385]"
+sudo -u www bash /opt/scripts/commit.sh $DOMAIN
+/usr/share/essay/alert/slack.sh "satellites" "changes commited by eugene.ryabchuk for satellite $DOMAIN, task [SA-7385]"
 EOT
