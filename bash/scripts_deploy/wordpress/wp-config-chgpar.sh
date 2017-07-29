a88ea89d8921bc6d425c50cdb71b67b9cb222614
Eugene <jeyraboy@gmail.com>
2017-07-29 13:27:35 -0400

    Update wp-config-chgpar.sh

--- bash/scripts_deploy/wordpress/wp-config-chgpar.sh
+++ bash/scripts_deploy/wordpress/wp-config-chgpar.sh
@@ -1,3 +1,4 @@
+cd /var/www/html/*
 DB_USER=`cat wp-config.php | grep "DB_USER"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
 /bin/sed -i "s/$DB_USER/root/" wp-config.php
 DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
@@ -5,6 +6,7 @@ DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -
 DB_HOST=`cat wp-config.php | grep "DB_HOST"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
 /bin/sed -i "s/$DB_HOST/localhost/" wp-config.php
 
-DOMAIN=`pwd | cut -d "/" -f 5 | sed 's/\./\-/g'`
-/usr/bin/mysql < $DOMAIN.sql
-rm -rf 
+/usr/bin/mysql < *.sql
+rm -rf *sql
+sudo -u www bash /opt/scripts/commit.sh best-editing-services.com
+usr/share/essay/alert/slack.sh "satellites" "changes commited by eugene.ryabchuk for satellite [SA-7385]
