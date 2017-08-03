c4401e3b3be988ec71998a2630a1f2ded7d52c91
Eugene <jeyraboy@gmail.com>
2017-08-03 11:00:43 -0400

    Update wp-config-chgpar.sh

--- bash/scripts_deploy/wordpress/wp-config-chgpar.sh
+++ bash/scripts_deploy/wordpress/wp-config-chgpar.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
+
 IP=$1
+DOMAIN=$2
+PATHE=/var/www/html/$DOMAIN
 
-cat <<'EOT' | ssh root@$IP /bin/bash
-cd /var/www/html/*
-DOMAIN=`pwd | cut -d "/" -f 5 | sed 's/\./\-/g'`
-DB_USER=`cat wp-config.php | grep "DB_USER"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
-/bin/sed -i "s/$DB_USER/root/" wp-config.php
-DB_PASSWORD=`cat wp-config.php | grep "DB_PASSWORD"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
-/bin/sed -i "s/$DB_PASSWORD/123123asd/" wp-config.php
-DB_HOST=`cat wp-config.php | grep "DB_HOST"  | awk "{print $2}"  | awk -F", '" '{print $2}' | awk -F"'" '{print $1}'`
-/bin/sed -i "s/$DB_HOST/localhost/" wp-config.php
+if [ -z "$IP" ]; then
+    echo "usage: ./install-db.sh <ip> <domain>"
+    exit
+fi
 
-/usr/bin/mysql < *.sql
-rm -rf *sql
+/bin/cat <<EOT | /usr/bin/ssh root@$IP /bin/bash
+cd $PATHE
+sed -i "/DB_HOST/s/'[^']*'/'localhost'/2" $PATHE/wp-config.php
+sed -i "/DB_USER/s/'[^']*'/'root'/2" $PATHE/wp-config.php
+sed -i "/DB_PASSWORD/s/'[^']*'/'123123asd'/2" $PATHE/wp-config.php
+
+/usr/bin/mysql < $PATHE/*.sql
+rm  $PATHE/*.sql
 sudo -u www bash /opt/scripts/commit.sh $DOMAIN
 /usr/share/essay/alert/slack.sh "satellites" "changes commited by eugene.ryabchuk for satellite $DOMAIN, task [SA-7385]"
 EOT
